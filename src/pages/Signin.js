99e94e0f54278c2b81d6091a086fb26585bf166f
xxyeon129 <xxyeon129@gmail.com>
2023-04-09 17:32:57 +0900

    [feat] 로그인 기본 구조,  유효성 검사 기능 구현

--- src/pages/Signin.js
+++ src/pages/Signin.js
@@ -1,3 +1,75 @@
+import { useState, useEffect } from "react";
+
 export default function Signin() {
-    return <div>signIn</div>;
+    // 이메일, 비밀번호
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    // 유효성 검사 관련 상태
+    const [validationEmail, setValidationEmail] = useState(false);
+    const [validationPW, setValidationPW] = useState(false);
+
+    // 이메일 입력
+    const handleInputEmail = (event) => {
+        const inputEmail = event.target.value;
+        setEmail(inputEmail);
+    };
+
+    // 비밀번호 입력
+    const handleInputPW = (event) => {
+        const inputPassword = event.target.value;
+        setPassword(inputPassword);
+    };
+
+    // 유효성 검사에 따른 버튼 disabled 조건 변경
+    useEffect(() => {
+        if (email.includes("@")) {
+            setValidationEmail(true);
+        } else {
+            setValidationEmail(false);
+        }
+
+        if (password.length >= 8) {
+            setValidationPW(true);
+        } else {
+            setValidationPW(false);
+        }
+    }, [email, password]);
+
+    // 로그인
+    const handleSubmit = (event) => {
+        event.preventDefault();
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <div>
+                <div id="email">
+                    <label htmlFor="email">이메일</label>
+                    <input
+                        data-testid="email-input"
+                        type="email"
+                        placeholder="이메일을 입력해주세요."
+                        onChange={handleInputEmail}
+                    />
+                </div>
+                <div id="password">
+                    <label>비밀번호</label>
+                    <input
+                        data-testid="password-input"
+                        type="password"
+                        placeholder="비밀번호를 8자 이상 입력해주세요."
+                        onChange={handleInputPW}
+                    />
+                </div>
+                <button
+                    data-testid="signup-button"
+                    type="submit"
+                    disabled={!(validationEmail && validationPW)}
+                >
+                    로그인
+                </button>
+            </div>
+        </form>
+    );
 }
