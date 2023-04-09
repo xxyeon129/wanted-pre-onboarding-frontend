85326383dcf9f0126c8645e8a6b05306167da832
xxyeon129 <xxyeon129@gmail.com>
2023-04-09 15:42:36 +0900

    [feat] 회원가입 기본 구조 구현, 이메일, 패스워드, 버튼 state 설정

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,3 +1,57 @@
+import { useState } from "react";
+
 export default function Signup() {
-    return <div>signUp</div>;
+    // 이메일, 비밀번호, 버튼 상태
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [btnDisabled, setBtnDisabled] = useState(true);
+
+    // 이메일 입력
+    const handleInputEmail = (event) => {
+        const inputEmail = event.target.value;
+        setEmail(inputEmail);
+    };
+
+    // 비밀번호 입력
+    const handleInputPW = (event) => {
+        const inputPassword = event.target.value;
+        setPassword(inputPassword);
+    };
+
+    // 버튼 클릭
+    const handleSubmit = (event) => {
+        event.preventDefault();
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <div>
+                <div id="email">
+                    <label htmlFor="email">이메일</label>
+                    <input
+                        data-testid="email-input"
+                        type="email"
+                        placeholder="이메일을 입력해주세요."
+                        onChange={handleInputEmail}
+                    />
+                </div>
+                <div id="password">
+                    <label>비밀번호</label>
+                    <input
+                        data-testid="password-input"
+                        type="password"
+                        placeholder="비밀번호를 8자 이상 입력해주세요."
+                        onChange={handleInputPW}
+                    />
+                </div>
+                <button
+                    data-testid="signup-button"
+                    onClick={handleSubmit}
+                    disabled={btnDisabled}
+                >
+                    회원가입
+                </button>
+            </div>
+        </form>
+    );
 }
