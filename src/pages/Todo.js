2ae38adc3cdae9fc5c297247ef30df65073df486
xxyeon129 <xxyeon129@gmail.com>
2023-04-11 22:22:49 +0900

    [feat] 투두리스트 HTML 기본 구조 설정

--- src/pages/Todo.js
+++ src/pages/Todo.js
@@ -1,3 +1,17 @@
 export default function Todo() {
-    return <div>TODO</div>;
+    return (
+        <div>
+            Add Todo
+            <input data-testid="new-todo-input" />
+            <button data-testid="new-todo-add-button">추가</button>
+            <li>
+                <label>
+                    <input type="checkbox" />
+                    <span>TODO 1</span>
+                </label>
+                <button data-testid="modify-button">수정</button>
+                <button data-testid="delete-button">삭제</button>
+            </li>
+        </div>
+    );
 }
