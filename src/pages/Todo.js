e7e1a8faab3bcd19f524beae3b69d2a1622967b9
xxyeon129 <xxyeon129@gmail.com>
2023-04-15 14:19:00 +0900

    [feat] Todo 추가 기능 구현

--- src/pages/Todo.js
+++ src/pages/Todo.js
@@ -1,9 +1,46 @@
+import axios from "axios";
+import { useState } from "react";
+
 export default function Todo() {
+    const [inputTodo, setInputTodo] = useState("");
+
+    // API
+    const url = "https://www.pre-onboarding-selection-task.shop";
+    // access token
+    const access_token = localStorage.getItem("loginToken");
+
+    const createTodo = (event) => {
+        event.preventDefault();
+
+        axios
+            .post(
+                `${url}/todos`,
+                { todo: inputTodo },
+                {
+                    headers: {
+                        Authorization: `Bearer ${access_token}`,
+                        "Content-Type": "application/json",
+                    },
+                }
+            )
+            .then(() => setInputTodo(""))
+            .catch((error) => alert(error.response.data.message));
+    };
+
     return (
         <div>
             Add Todo
-            <input data-testid="new-todo-input" />
-            <button data-testid="new-todo-add-button">추가</button>
+            <form onSubmit={createTodo}>
+                <input
+                    data-testid="new-todo-input"
+                    placeholder="새로운 할 일을 입력해주세요."
+                    onChange={(event) => setInputTodo(event.target.value)}
+                    value={inputTodo}
+                />
+                <button data-testid="new-todo-add-button" type="submit">
+                    추가
+                </button>
+            </form>
             <li>
                 <label>
                     <input type="checkbox" />
