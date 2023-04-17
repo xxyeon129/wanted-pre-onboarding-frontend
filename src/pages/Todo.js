0532abed1138bccd3038c2e0328a5d2a9df2b9e0
xxyeon129 <xxyeon129@gmail.com>
2023-04-18 01:05:02 +0900

    [refactor] todo API 관련 코드 리팩토링

--- src/pages/Todo.js
+++ src/pages/Todo.js
@@ -1,76 +1,54 @@
-import axios from "axios";
 import { useEffect, useState } from "react";
 import TodoItem from "../components/TodoItem";
+import { TodoAPI } from "../API/ServerAPI";
 
 export default function Todo() {
     // 입력 input, 입력한 To Do List 관련 상태
     const [inputTodo, setInputTodo] = useState("");
     const [todoList, setTodoList] = useState([]);
 
-    // API
-    const url = process.env.REACT_APP_SERVER_URL;
-    // access token
-    const access_token = localStorage.getItem("loginToken");
-
     // To Do List 렌더링
-    const getTodo = () => {
-        axios
-            .get(`${url}/todos`, {
-                headers: { Authorization: `Bearer ${access_token}` },
-            })
-            .then((res) => setTodoList(res.data));
+    const getTodo = async () => {
+        try {
+            const res = await TodoAPI.getTodo();
+            setTodoList(res.data);
+        } catch (error) {
+            alert(error.response.data.message);
+        }
     };
 
     // To Do 추가
-    const createTodo = (event) => {
+    const createTodo = async (event) => {
         event.preventDefault();
 
-        axios
-            .post(
-                `${url}/todos`,
-                { todo: inputTodo },
-                {
-                    headers: {
-                        Authorization: `Bearer ${access_token}`,
-                        "Content-Type": "application/json",
-                    },
-                }
-            )
-            .then(() => setInputTodo(""))
-            .catch((error) => alert(error.response.data.message));
+        try {
+            await TodoAPI.createTodo(inputTodo);
+            setInputTodo("");
+        } catch (error) {
+            alert(error.response.data.message);
+        }
     };
 
     // To Do 수정
-    const updateTodo = (id, updatedTodo, updatedCheck) => {
-        axios
-            .put(
-                `${url}/todos/${id}`,
-                { todo: updatedTodo, isCompleted: updatedCheck },
-                {
-                    headers: {
-                        Authorization: `Bearer ${access_token}`,
-                        "Content-Type": "application/json",
-                    },
-                }
-            )
-            .then((res) => {
-                const newTodoList = todoList.map((todo) =>
-                    todo.id === id ? res.data : todo
-                );
-                setTodoList(newTodoList);
-            })
-            .catch((error) => alert(error.response.data.message));
+    const updateTodo = async (id, updatedTodo, updatedCheck) => {
+        try {
+            const res = await TodoAPI.updateTodo(id, updatedTodo, updatedCheck);
+            const newTodoList = todoList.map((todo) =>
+                todo.id === id ? res.data : todo
+            );
+            setTodoList(newTodoList);
+        } catch (error) {
+            alert(error.response.data.message);
+        }
     };
 
     // To Do 삭제
-    const deleteTodo = (id) => {
-        axios
-            .delete(`${url}/todos/${id}`, {
-                headers: {
-                    Authorization: `Bearer ${access_token}`,
-                },
-            })
-            .catch((error) => alert(error.response.data.message));
+    const deleteTodo = async (id) => {
+        try {
+            await TodoAPI.deleteTodo(id);
+        } catch (error) {
+            alert(error.response.data.message);
+        }
     };
 
     useEffect(() => {
