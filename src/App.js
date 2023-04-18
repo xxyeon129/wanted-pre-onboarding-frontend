964a2008078e04439771a82a62f3931f7e77a855
xxyeon129 <xxyeon129@gmail.com>
2023-04-18 12:00:50 +0900

    [feat] 루트 경로 페이지 라우터 설정

--- src/App.js
+++ src/App.js
@@ -29,6 +29,16 @@ export default function App() {
             <Router>
                 <Routes>
                     <Route
+                        path="/"
+                        element={
+                            isLogin ? (
+                                <Navigate to="/todo" />
+                            ) : (
+                                <Navigate to="/signin" />
+                            )
+                        }
+                    />
+                    <Route
                         path="/signin"
                         element={
                             isLogin ? (
